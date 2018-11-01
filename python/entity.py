015d1849c93a049fd6bda55db75602bd14abc20b
Mathew <mathew@192.168.1.106>
2018-11-01 15:46:24 +0200

    Done

--- python/entity.py
+++ python/entity.py
@@ -1,131 +1,176 @@
-import psycopg2
+import psycopg2.extras
+
 
 class DatabaseError(Exception):
     pass
+
+
 class NotFoundError(Exception):
     pass
+
+
+class ModifiedError(Exception):
+    pass
+
 
 class Entity(object):
     db = None
 
-    # ORM part 1
-    __delete_query    = 'DELETE FROM "{table}" WHERE {table}_id=%s'
-    __insert_query    = 'INSERT INTO "{table}" ({columns}) VALUES ({placeholders}) RETURNING "{table}_id"'
-    __list_query      = 'SELECT * FROM "{table}"'
-    __select_query    = 'SELECT * FROM "{table}" WHERE {table}_id=%s'
-    __update_query    = 'UPDATE "{table}" SET {columns} WHERE {table}_id=%s'
+    try:
+        db = psycopg2.connect(user='mathew', password='root', dbname='shop', host='localhost', port='5433')
+    except Exception:
+        raise DatabaseError()
 
-    # ORM part 2
-    __parent_query    = 'SELECT * FROM "{table}" WHERE {parent}_id=%s'
-    __sibling_query   = 'SELECT * FROM "{sibling}" NATURAL JOIN "{join_table}" WHERE {table}_id=%s'
-    __update_children = 'UPDATE "{table}" SET {parent}_id=%s WHERE {table}_id IN ({children})'
+    __delete_query = 'DELETE FROM "{table}" WHERE {table}_id=%s'
+    __insert_query = 'INSERT INTO "{table}" ({columns}) VALUES ({placeholders}) RETURNING "{table}_id"'
+    __list_query = 'SELECT * FROM "{table}"'
+    __select_query = 'SELECT * FROM "{table}" WHERE "{table}_id"=%s'
+    __update_query = 'UPDATE "{table}" SET {columns} WHERE {table}_id=%s'
+    __select_query2 = 'SELECT {columns} FROM {table} WHERE {table}_id=%s'
+    __count_query = 'SELECT COUNT(*) FROM "{table}"'
 
     def __init__(self, id=None):
         if self.__class__.db is None:
             raise DatabaseError()
 
-        self.__cursor   = self.__class__.db.cursor(
+        self.__cursor = self.__class__.db.cursor(
             cursor_factory=psycopg2.extras.DictCursor
         )
-        self.__fields   = {}
-        self.__id       = id
-        self.__loaded   = False
+        self.__fields = {}
+        self.__id = id
+        self.__loaded = False
         self.__modified = False
-        self.__table    = self.__class__.__name__.lower()
+        self.__table = self.__class__.__name__.lower()
 
     def __getattr__(self, name):
-        # check, if instance is modified and throw an exception
-        # get corresponding data from database if needed
-        # check, if requested property name is in current class
-        #    columns, parents, children or siblings and call corresponding
-        #    getter with name as an argument
-        # throw an exception, if attribute is unrecognized
-        pass
+        if name in ('_Entity__cursor', '_Entity__fields', '_Entity__loaded', '_Entity__modified', '_Entity__table'):
+            raise AttributeError()
+        if self.__modified:
+            raise ModifiedError()
+        if not self.__loaded:
+            self.__load()
+
+        value = self.__fields.get(name)
+        if value is None:
+            raise AttributeError()
+        return value
 
     def __setattr__(self, name, value):
-        # check, if requested property name is in current class
-        #    columns, parents, children or siblings and call corresponding
-        #    setter with name and value as arguments or use default implementation
-        pass
+        if not (hasattr(self, '_Entity__table')) or name in \
+                ('_Entity__cursor',
+                 '_Entity__fields',
+                 '_Entity__loaded',
+                 '_Entity__modified',
+                 '_Entity__table',):
+            self.__dict__[name] = value
+        else:
+            self._set_column(name, value)
+            self.__modified = True
 
-    def __execute_query(self, query, *args):
-        # execute an sql statement and handle exceptions together with transactions
-        pass
+    def __execute_query(self, query, args=None):
+        try:
+            if not(args is None):
+                self.__cursor.execute(query, args)
+            else:
+                self.__cursor.execute(query)
+        except Exception:
+            Entity.db.rollback()
+            raise DatabaseError()
+        Entity.db.commit()
+        # print(query)
+
+        try:
+           fetch = self.__cursor.fetchone()
+        except Exception:
+            return None
+        return fetch
 
     def __insert(self):
-        # generate an insert query string from fields keys and values and execute it
-        # use prepared statements
-        # save an insert id
-        pass
+        if self.__id is None:
+            gap = ", "
+            keys = self.__fields.keys()
+            values = self.__fields.values()
+            values2 = []
+            for i in values:
+                values2.append(f"'{i}'")
+            self.__id = self.__execute_query(Entity.__insert_query.format(table=self.__table,
+                                                                          columns=gap.join(keys),
+                                                                          placeholders=gap.join(values)))
 
     def __load(self):
-        # if current instance is not loaded yet — execute select statement and store it's result as an associative array (fields), where column names used as keys
-        pass
+        if not self.__loaded and not(self.__id is None):
+            values = self.__execute_query(Entity.__select_query.format(table=self.__table), (self.__id,))
 
-    def __update(self):
-        # generate an update query string from fields keys and values and execute it
-        # use prepared statements
-        pass
+            self.__fields.update(values)
+            self.__loaded = True
 
-    def _get_children(self, name):
-        # return an array of child entity instances
-        # each child instance must have an id and be filled with data
-        pass
+    def __update(self):  # !!!!!!!!!!!!!!
+        if not (self.id is None):
+            gap = ', '
+            keys = list(self.__fields.keys())
+            values = list(self.__fields.values())
+            columns = []
+
+            i = 0
+            while i < len(keys):
+                columns.append(f"{keys[i]}='{values[i]}'")
+                i += 1
+
+            self.__execute_query(Entity.__update_query.format(table=self.__table, columns=gap.join(columns)), (self.__id,))
 
     def _get_column(self, name):
-        # return value from fields array by <table>_<name> as a key
-        pass
-
-    def _get_parent(self, name):
-        # ORM part 2
-        # get parent id from fields with <name>_id as a key
-        # return an instance of parent entity class with an appropriate id
-        pass
-
-    def _get_siblings(self, name):
-        # ORM part 2
-        # get parent id from fields with <name>_id as a key
-        # return an array of sibling entity instances
-        # each sibling instance must have an id and be filled with data
-        pass
+        return self.__fields[f'{self.__table}_{name}']
 
     def _set_column(self, name, value):
-        # put new value into fields array with <table>_<name> as a key
-        pass
-
-    def _set_parent(self, name, value):
-        # ORM part 2
-        # put new value into fields array with <name>_id as a key
-        # value can be a number or an instance of Entity subclass
-        pass
+        self.__fields[name] = value
 
     @classmethod
     def all(cls):
-        # get ALL rows with ALL columns from corrensponding table
-        # for each row create an instance of appropriate class
-        # each instance must be filled with column data, a correct id and MUST NOT query a database for own fields any more
-        # return an array of istances
-        pass
+        instances = []
+
+        temp_cursor = cls.db.cursor(
+            cursor_factory=psycopg2.extras.DictCursor
+        )
+
+        temp_cursor.execute(cls.__count_query.format(table=cls.__name__.lower()))
+        limit = temp_cursor.fetchone()[0]
+
+        i = 1
+        while i <= limit:
+            instances.append(cls(i))
+            i += 1
+
+        return instances
 
     def delete(self):
-        # execute delete query with appropriate id
-        pass
+        if self.__id is None:
+            raise RuntimeError()
+        else:
+            self.__execute_query(Entity.__delete_query.format(table=self.__table), (self.__id,))
+            self.__id = None
 
     @property
     def id(self):
-        # try to guess yourself
-        pass
+        return self.__id
 
     @property
     def created(self):
-        # try to guess yourself
-        pass
+        return self.__execute_query(Entity.__select_query2.format(columns=f"{self.__table}_created", table=self.__table), (self.__id,))[0]
 
     @property
     def updated(self):
-        # try to guess yourself
-        pass
+        return self.__execute_query(Entity.__select_query2.format(columns=f"{self.__table}_updated", table=self.__table), (self.__id,))[0]
 
     def save(self):
-        # execute either insert or update query, depending on instance id
-        pass
+        if self.__id is None:
+            self.__insert()
+            self.__modified = False
+        else:
+            self.__update()
+            self.__modified = False
+
+
+# class Section(Entity):
+#     _columns = ['title']
+
+
